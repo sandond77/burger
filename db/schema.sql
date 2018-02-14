534c6fd26b4a74d105c0cd5d7d4ac52f38d55a2f
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-02-13 23:35:27 -0800

    added primary key to schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,8 @@ USE burgers_db;
 CREATE TABLE burgers (
 id INT(255) auto_increment,
 burger_name VARCHAR(255) not null,
-devoured BOOLEAN not null default 0
+devoured BOOLEAN not null default 0,
+PRIMARY KEY(id)
 );
 
 SELECT * FROM burgers; 
