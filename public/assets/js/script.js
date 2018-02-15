316b37f59838fdb4193650f35196d600230fb7a8
sandond77 <sandu@ucdavis.edu>
2018-02-14 23:25:34 -0800

    assignment complete

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,13 +4,31 @@ $(function(){
 		var update ={
 			devoured: 1
 		}
-		console.log("the id is " + id)
+
 	    $.ajax("/api/burgers/" + id, {
       		type: "PUT",
       		data: update
     	}).then(
       		function() {
         		console.log("Burger Has been devoured");
+        		location.reload();
+      		}
+    	);
+	})
+
+	$(".submit").click(function(event){
+		var name = $("#burger").val().trim();
+		console.log("name: " + name)
+		var add = {
+			name: name
+		}
+
+	    $.ajax("/api/burgers/", {
+      		type: "POST",
+      		data: add
+    	}).then(
+      		function() {
+        		console.log("Added a new burger");
         		location.reload();
       		}
     	);
